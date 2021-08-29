15fdbe3fe0ee18733e4599785e01fb6fc52d3751
emmanoel <tecnologia@atijuridico.com.br>
2021-08-29 00:12:34 -0300

    Correções simples

--- src/test/java/com/aws/managers3/Managers3ApplicationTests.java
+++ src/test/java/com/aws/managers3/Managers3ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Managers3ApplicationTests {
+public class Managers3ApplicationTests {
 
 	@Test
 	void contextLoads() {
